4c71c4e29d80f42c3df95e417ad7240f4ed5e3c9
Venice Vargas <venicevargas1@live.com>
2020-09-08 01:07:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,38 +1,14 @@
 class Shoe
+  attr_accessor :color, :size, :material
+  
+  attr_accessor :brand  
   
   def initialize(brand)
     @brand=brand
   end
     
-    def brand
-      @brand
-    end
-    
-    def color=(color)
-      @color=color
-    end
-    
-    def color
-      @color
-    end
-    
-    def size=(num)
-      @size=num
-    end
-    
-    def size 
-      @size
-    end
-    
-    def material=(material)
-      @material=material
-    end
-    
-    def material
-      @material
-    end
-    
-    def condition=(new)
+  
+  def condition=(new)
       
     end
   end
